15bef8f9e57c09e0f8d67d830782d9dfaef341ef
Marco Tini <info@marcotini.com>
2017-04-06 09:15:22 +0200

    Update from corbinmunce

--- hardware/XiaomiGateway.h
+++ hardware/XiaomiGateway.h
@@ -11,11 +11,12 @@ public:
 	~XiaomiGateway(void);
 	bool WriteToHardware(const char *pdata, const unsigned char length);
 	bool SendMessageToGateway(const std::string &controlmessage);
-	void InsertUpdateSwitch(const std::string &nodeid, const std::string &Name, const bool bIsOn, const _eSwitchType switchtype, const int level, const bool isctlr2 = false, const bool is2ndchannel = false);
+	void InsertUpdateSwitch(const std::string &nodeid, const std::string &Name, const bool bIsOn, const _eSwitchType switchtype, const int level, const std::string &messagetype, const bool isctlr2, const bool is2ndchannel, const std::string &load_power, const std::string &power_consumed, const int battery);
 	void InsertUpdateCubeText(const std::string &nodeid, const std::string &Name, const std::string &degrees);
 	void InsertUpdateVoltage(const std::string &nodeid, const std::string &Name, const int VoltageLevel);
-	void InsertUpdateTemperature(const std::string &nodeid, const std::string &Name, const float Temperature);
-	void InsertUpdateHumidity(const std::string &nodeid, const std::string &Name, const int Humidity);
+	void InsertUpdateLux(const std::string &nodeid, const std::string &Name, const int Illumination);
+	void InsertUpdateTemperature(const std::string &nodeid, const std::string &Name, const float Temperature, const int battery);
+	void InsertUpdateHumidity(const std::string &nodeid, const std::string &Name, const int Humidity, const int battery);
 	void InsertUpdateRGBGateway(const std::string &nodeid, const std::string &Name, const bool bIsOn, const int brightness, const int hue);
 	void UpdateToken(const std::string &value);
 
